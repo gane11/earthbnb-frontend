0980ca1a0d0797b3af1159904eb3052bfb2e0fff
aleksandar <aca.djordjevic1113@gmail.com>
2021-01-12 14:56:16 -0800

    styling the reservation container

--- earthbnbfrontend/src/components/Reservations.js
+++ earthbnbfrontend/src/components/Reservations.js
@@ -35,13 +35,13 @@ const Reservations = ({home}) =>{
   return (
    <>
     <div className="reservation_container">
-      <h2>{`${home.price}/night`}</h2>
       <form onSubmit={submitHandler}>
-        <p>CHECK-IN</p>
+      <h2>{`${home.price}/night`}</h2>
+        <h4>CHECK-IN</h4>
         <input onChange={updateProperty(setCheckIn)} type="date"></input>
-        <p>CHECK-OUT</p>
+        <h4>CHECK-OUT</h4>
         <input onChange={updateProperty(setCheckOut)} type="date"></input>
-        <p>GUESTS</p>
+        <h4>GUESTS</h4>
         {/* <label for="num_guests">GUESTS</label> */}
         <select onChange={updateProperty(setNumGuests)} name="num_guests" id="num_guests">
           <option value="num_1">1</option>
@@ -50,7 +50,7 @@ const Reservations = ({home}) =>{
           <option value="num_1">4</option>
           <option value="num_1">5</option>
         </select>
-        <p>Price: $ {homePrice}</p>
+        <h3>Price: $ {homePrice}</h3>
         <Button type="submit" variant="contained" color="secondary"
         >Check avaliability</Button>
       </form>
